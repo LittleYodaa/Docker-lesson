a56ca9fd391fbd3899840bc09d8cfdaf387f5fbf
Patryk Kawula <kawul1996@gmail.com>
2024-01-12 12:44:30 +0100

    Change filename in build-and-restart.sh

--- build-and-restart.sh
+++ build-and-restart.sh
@@ -1,4 +1,4 @@
-docker build . -t Docker-lesson
-docker stop Docker-lesson
-docker rm Docker-lesson
-docker run -d -p 8080:8080 --name=Docker-lesson Docker-lesson
+docker build . -t dockerlesson
+docker stop dockerlesson
+docker rm dockerlesson
+docker run -d -p 8080:8080 --name=dockerlesson dockerdlesson
