dedb2f7be9e885cbd414793b1b6a060faa9b2a47
Patryk Kawula <kawul1996@gmail.com>
2024-01-13 00:28:48 +0100

    Change filename in Dockerfile

--- build-and-restart.sh
+++ build-and-restart.sh
@@ -1,4 +1,4 @@
 docker build . -t dockerlesson
 docker stop dockerlesson || true
 docker rm dockerlesson || true
-docker run -d -p 8080:8080 --name=dockerlesson dockerdlesson
+docker run -d -p 8080:8080 --name=dockerlesson dockerlesson
