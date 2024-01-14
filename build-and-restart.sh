9edcaada5daa206082347ca36def3fe8a57fa169
Patryk Kawula <kawul1996@gmail.com>
2024-01-14 12:06:33 +0100

    Add mysql database and liquibase

--- build-and-restart.sh
+++ build-and-restart.sh
@@ -1,4 +1,4 @@
 docker build . -t dockerlesson
 docker stop dockerlesson || true
 docker rm dockerlesson || true
-docker run -d -p 8080:8080 --name=dockerlesson dockerlesson
+docker run -d -p 8080:8080 --name=dockerlesson  -e SPRING_PROFILES_ACTIVE=prod --network dockerlesson-network --restart unless-stopped dockerlesson
